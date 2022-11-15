1f9ffd080c66e7b11ea1763cb15d3c55b8274907
gabrielver <verronet.g@gmail.com>
2022-11-15 19:34:56 +0100

    mobil first ok

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,8 +4,95 @@ type Props = {}
 
 function Contact({}: Props) {
   return (
-    <div id='contact' className='relative flex items-center justify-center min-h-screen bg-[#F2EBD6]'>
-        <div className='text-7xl'>Contact</div>
+    <div id='contact' className='relative flex flex-col items-center min-h-screen bg-[#F2EBD6]'>
+        <div className='contact_title'>
+          <h1>Points de Ventes</h1>
+          <h2>Retrouvez tous nos produits chez nos partenaires distributeurs</h2>
+        </div>
+        <div className='banderole'>
+          <div className="banderole_inside">
+            leBonMarche-Biocop-Naturalia-LaGrandeEpicer
+          </div>
+        </div>
+        <div className='contact_list'>
+            <div className="template ">
+              <div className="address">
+              <h3>le bon marché</h3>
+              <h4>Paris 75009</h4>
+              <h5>9 rue ds martyrs</h5>
+              </div>
+              <a href="" className="tel">01 71 73 43 77</a>
+              <div className="hour">
+                  <span>lundi au vendredi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>samedi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>dimanche</span>
+                  <p>11h-14h & 15h-19h30</p>
+              </div>
+              <a href="">site web</a>
+            </div>
+            <div className="template ">
+              <div className="address">
+              <h3>le bon marché</h3>
+              <h4>Paris 75009</h4>
+              <h5>9 rue ds martyrs</h5>
+              </div>
+              <a href="" className="tel">01 71 73 43 77</a>
+              <div className="hour">
+                  <span>lundi au vendredi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>samedi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>dimanche</span>
+                  <p>11h-14h & 15h-19h30</p>
+              </div>
+              <a href="">site web</a>
+            </div>
+            
+        </div>
+        <div className='contact_list'>
+        <div className="template ">
+              <div className="address">
+              <h3>le bon marché</h3>
+              <h4>Paris 75009</h4>
+              <h5>9 rue ds martyrs</h5>
+              </div>
+              <a href="" className="tel">01 71 73 43 77</a>
+              <div className="hour">
+                  <span>lundi au vendredi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>samedi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>dimanche</span>
+                  <p>11h-14h & 15h-19h30</p>
+              </div>
+              <a href="">site web</a>
+            </div>
+            <div className="template ">
+              <div className="address">
+              <h3>le bon marché</h3>
+              <h4>Paris 75009</h4>
+              <h5>9 rue ds martyrs</h5>
+              </div>
+              <a href="" className="tel">01 71 73 43 77</a>
+              <div className="hour">
+                  <span>lundi au vendredi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>samedi</span>
+                  <p>11h-14h & 15h-19h30</p>
+                  <span>dimanche</span>
+                  <p>11h-14h & 15h-19h30</p>
+              </div>
+              <a href="">site web</a>
+            </div>
+            
+        </div>
+        <button className='more'>voir plus</button>
+        <div className="info_sup">
+          <p>Pour tout renseignements ou pour devenir l'un de nos point de vente, vous pouvez nous contacter a l'adresse suivante :</p>
+          <a href="">info@lesconfituredepascal.fr</a>
+        </div>
     </div>
   )
 }
--- components/Products.tsx
+++ components/Products.tsx
@@ -5,51 +5,58 @@ type Props = {}
 
 function Products({}: Props) {
   return (
-    <div id='product' className='relative flex min-h-screen '>
+    <div id='product' className='relative flex min-h-screen overflow-hidden '>
       <div className="bg flex-col justify-center items-center">
-      <div className="product_text text-center">
+      <div className="product_text text-center pt-8">
         <h1>Nos délicieuses confitures</h1>
-        <h2>Nous travaillons avec des fruits bio, et n’hésitons pas à sublimer les fruits avec un soupçon d’épices bio sélectionnées et commercialisées par un grand nom de la gastronomie française.
+        <h2>Nous travaillons avec des fruits bio, et n’hésitons pas à sublimer les fruits avec un soupçon d’épices (également) bio .
             Les fruits sont sélectionnés avec soin, Pascal se déplace sur le marché de Rungis chaque semaine et nous recherchons des producteurs locaux ou des régions avoisinantes (Normandie, Picardie, ...)
         </h2>
       </div>
-      <div className="cards flex flex-col justify-center pl-4 pr-4 ">
-        <div className="card rouge relative  ">
-            <div className="ellipse bg-red-500 w-1/3 flex items-center justify-center overflow-hidden"  >
-              <Image className='flex scale-50 w' src="/cerise.png" alt="pot" height={100} width={100} />
+      <div className="cards flex flex-col justify-between pl-4 pr-4 ">
+       <div className="cards_up flex justify-around items-center pt-8">
+       <div className="card rouge relative  ">
+            <div className="ellipse bg-[#fe353530] rounded-full  flex items-center justify-center h-36 w-36 "  >
+              <Image className='flex scale-[2.5] mix-blend-multiply' src="/fruit_rouge.png" alt="pot" height={100} width={100} />
             </div>
-            <button className="product_title bg-white rounded-md p-1 absolute bottom-2 ">
+            <button className="product_title  w-full rounded-b-sm p-1 mt-3 ">
               <p>Fruits Rouge</p>
             </button>
+            {/* <Image className='absolute scale-100 left-[-50%] top-[20%] rotate-[-25deg]' src="/cerise.png" alt="cerise"  height={100} width={100} /> */}
         </div>
-        <Image className='absolute left-9 bottom-96' src="/cerise.png" alt="cerise"  height={100} width={100} />
-        <div className="card green relative flex justify-end">
-            <div className="ellipse bg-green-500 w-1/3  flex items-center justify-center overflow-hidden"  >
-              <Image className='flex scale-50 w' src="/cerise.png" alt="pot" height={100} width={100} />
+        <div className="card verger relative min-w-[120px] ">
+            <div className="ellipse bg-yellow-100 rounded-full flex items-center justify-center h-36 w-36  "  >
+              <Image className='flex scale-[2.5] mix-blend-luminosity' src="/_verger.png" alt="pot" height={100} width={100} />
             </div>
-            <button className="product_title bg-white rounded-md p-1 absolute bottom-2 ">
-              <p>Fruits Rouge</p>
+            <button className="product_title w-full rounded-b-sm p-1 mt-3  ">
+              <p>Fruits du Verger</p>
             </button>
+            {/* <Image className='absolute scale-125 right-[-40%] top-[30%] rotate-12' src="/pomme.png" alt="cerise"  height={100} width={100} /> */}
         </div>
-        <Image className='absolute right-9 bottom-60' src="/cerise.png" alt="cerise"  height={100} width={100} />
-        <div className="card yellow relative ">
-            <div className="ellipse bg-yellow-500 w-full  flex items-center justify-center overflow-hidden"  >
-              <Image className='flex scale-50 w' src="/cerise.png" alt="pot" height={100} width={100} />
+        
+       </div>
+       <div className="cards_down flex justify-around items-center pt-20">
+       <div className="card rouge relative min-w-[120px]">
+            <div className="ellipse bg-orange-200 rounded-full flex items-center justify-center h-36 w-36  "  >
+              <Image className='flex scale-[2.5] mix-blend-luminosity' src="/_agrumes.png" alt="pot" height={100} width={100} />
             </div>
-            <button className="product_title bg-white rounded-md p-1 absolute bottom-2 ">
-              <p>Fruits Rouge</p>
+            <button className="product_title w-full rounded-b-sm p-1 mt-3 ">
+              <p>Agrumes</p>
             </button>
+            {/* <Image className='absolute scale-125 left-[-40%] bottom-[10%] rotate-[-20deg]' src="/orange.png" alt="cerise"  height={100} width={100} /> */}
         </div>
-        <Image className='absolute left-9 bottom-24' src="/cerise.png" alt="cerise"  height={100} width={100} />
-        <div className="card blue relative ">
-            <div className="ellipse bg-blue-500 w-full  flex items-center justify-center overflow-hidden"  >
-              <Image className='flex scale-50 w' src="/cerise.png" alt="pot" height={100} width={100} />
+        <div className="card green relative min-w-[120px] ">
+            <div className="ellipse bg-[#d970ff2f] rounded-full flex items-center justify-center h-36 w-36   "  >
+              <Image className='flex scale-[2.5] mix-blend-luminosity' src="/_meli_melo.png" alt="pot" height={100} width={100} />
             </div>
-            <button className="product_title bg-white rounded-md p-1 absolute bottom-2 ">
-              <p>Fruits Rouge</p>
+            <button className="product_title w-full rounded-b-sm p-1 mt-3  ">
+              <p>Méli-Mélo</p>
             </button>
+            {/* <Image className='absolute right-[60%] bottom-[10%] drop-shadow-md' src="/myrtille.png" alt="cerise"  height={100} width={100} />
+            <Image className='absolute right-[-39%] bottom-[10%] -rotate-180 drop-shadow-md' src="/myrtille.png" alt="cerise"  height={100} width={100} /> */}
         </div>
-        <Image className='absolute right-9 bottom-0' src="/cerise.png" alt="cerise"  height={100} width={100} />
+        
+       </div>
       </div>
       </div>
    
